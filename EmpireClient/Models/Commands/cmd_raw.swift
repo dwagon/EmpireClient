41de42032d14cd8371936c3032370ae9cc254d55
Dougal Scott <dougal.scott@gmail.com>
2026-08-18 12:29:20 +1000

    Try and fix log view

---
+++ EmpireClient/Models/Commands/cmd_raw.swift
@@ -0,0 +1,8 @@
+//
+//  cmd_raw.swift
+//  EmpireClient
+//
+//  Created by Dougal Scott on 18/8/2026.
+//
+
+import Foundation
---
+++ EmpireClient/Views/RawCmdView.swift
@@ -0,0 +1,18 @@
+//
+//  RawCmdView.swift
+//  EmpireClient
+//
+//  Created by Dougal Scott on 18/8/2026.
+//
+
+import SwiftUI
+
+struct RawCmdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RawCmdView()
+}
