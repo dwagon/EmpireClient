5bfa502e1a3fbd7816e4454c7c1085e038b123cc
Dougal Scott <dougal.scott@gmail.com>
2026-08-23 09:36:30 +1000

    Implement nation report in a menu

--- EmpireClient/EmpireClientApp.swift
+++ EmpireClient/EmpireClientApp.swift
@@ -10,12 +10,26 @@ import SwiftUI
 @main
 struct EmpireClientApp: App {
     var game = Game()
+    @Environment(\.openWindow) private var openWindow
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView(game: game, center_coord: MapCoord(x: 0, y: 0))
         }
         Settings {
             SettingsView()
+        }
+        .commands {
+            CommandMenu("Reports") {
+                Button("Nation", systemImage: "popcorn") {
+                    openWindow(id: "nation_report")
+                }
+            }
+        }
+
+        Window("Nation Report", id: "nation_report") {
+            NationReportView(game: game)
         }
     }
 }
---
+++ EmpireClient/Views/NationReportView.swift
@@ -0,0 +1,33 @@
+//
+//  NationReportView.swift
+//  EmpireClient
+//
+//  Created by Dougal Scott on 22/8/2026.
+//
+
+import SwiftUI
+
+struct NationReportView: View {
+    let game: Game
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        VStack {
+            Text(game.nation_report.joined(separator: "\n"))
+                .font(
+                    .system(.body, design: .monospaced)
+                ).border(.blue)
+            HStack {
+                Button("OK") {
+                    dismiss()
+                }
+            }
+        }
+
+    }
+}
+
+#Preview {
+    @Previewable @State var game: Game = Game()
+    NationReportView(game: game)
+}
