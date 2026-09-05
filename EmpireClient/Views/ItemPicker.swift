473f24a5fcc0bef8ce889ca9422a0d18cc5650c4
Dougal Scott <dougal.scott@gmail.com>
2026-09-05 11:53:47 +1000

    Allow ItemPicker to have a subset of items

--- EmpireClient/Views/ItemPicker.swift
+++ EmpireClient/Views/ItemPicker.swift
@@ -3,29 +3,52 @@
 //  EmpireClient
 //
 //  Created by Dougal Scott on 29/8/2026.
-//
+//  Pick from the various items
 
 import SwiftUI
 
 struct ItemPicker: View {
     var label: String
+    var itemList: [Item]
     @Binding var item: Item
+
+    init(label: String, itemList: [Item], item: Binding<Item>) {
+        self.label = label
+        self.itemList = itemList
+        _item = item
+    }
+
+    init(label: String, item: Binding<Item>) {
+        self.label = label
+        _item = item
+        self.itemList = Item.allCases
+    }
 
     var body: some View {
         Picker(
             label,
             selection: $item,
             content: {
-                ForEach(Item.allCases, id: \.self) { item in
+                ForEach(itemList, id: \.self) { item in
                     Text(item.displayName.capitalized).tag(item)
                 }
             }
         )
         .pickerStyle(.menu)
+        .padding()
     }
 }
 
 #Preview {
     @Previewable @State var item: Item = .none
     ItemPicker(label: "Do something", item: $item)
+}
+
+#Preview("Subset") {
+    @Previewable @State var item: Item = .none
+    ItemPicker(
+        label: "Do something",
+        itemList: [Item.civ, Item.mil],
+        item: $item
+    )
 }
