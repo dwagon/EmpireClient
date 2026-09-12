97e44d123f80c96c40e907839d3da216734dc144
Dougal Scott <dougal.scott@gmail.com>
2026-09-12 10:32:58 +1000

    Make obvious when a ship is not buildable https://github.com/dwagon/EmpireClient/issues/76

--- EmpireClient/EmpireClientApp.swift
+++ EmpireClient/EmpireClientApp.swift
@@ -49,7 +49,7 @@ struct EmpireClientApp: App {
         }
 
         Window("Ship Types Report", id: "ship_types_report") {
-            ShipTypeReport(shipTypes: game.shipTypes)
+            ShipTypeReport(shipTypes: game.shipTypes, buildable: game.isShipBuildable)
         }
 
         Window("Land Unit Types Report", id: "land_types_report") {
--- EmpireClient/Views/Reports/ShipTypeReport.swift
+++ EmpireClient/Views/Reports/ShipTypeReport.swift
@@ -7,26 +7,62 @@
 
 import SwiftUI
 
+struct BuildHighlight: ViewModifier {
+    var canBuild: Bool
+
+    func body(content: Content) -> some View {
+        content.foregroundStyle(
+            canBuild ? .primary : .secondary
+        )
+    }
+}
+
 struct ShipTypeReport: View {
     let shipTypes: [String: ShipType]
+    let buildable: (String) -> Bool
     @Environment(\.dismiss) private var dismiss
     @State private var selectedShip: ShipType.ID?
 
     var body: some View {
         VStack {
             Table(Array(shipTypes.values), selection: $selectedShip) {
-                TableColumn("Abbrev", value: \.abbrev)
-                    .width(min: 30, ideal: 50, max: 60)
-                TableColumn("Name", value: \.name)
-                TableColumn("Speed") { details in Text("\(details.speed)") }
-                    .width(min: 40, ideal: 50, max: 60)
-                TableColumn("Capabilities", value: \.capabilities)
+                TableColumn("Abbrev") { details in
+                    Text("\(details.abbrev)")
+                        .modifier(
+                            BuildHighlight(canBuild: buildable(details.abbrev))
+                        )
+                }.width(min: 30, ideal: 50, max: 60)
+                TableColumn("Name") { details in
+                    Text("\(details.name)")
+                        .modifier(
+                            BuildHighlight(canBuild: buildable(details.abbrev))
+                        )
+                }
+                TableColumn("Speed") { details in
+                    Text("\(details.speed)").modifier(
+                        BuildHighlight(canBuild: buildable(details.abbrev))
+                    )
+                }
+                .width(min: 40, ideal: 50, max: 60)
+                TableColumn("Tech") { details in
+                    Text("\(details.tech)").modifier(
+                        BuildHighlight(canBuild: buildable(details.abbrev))
+                    )
+                }
+                .width(min: 40, ideal: 50, max: 60)
+                TableColumn("Capabilities") {
+                    details in
+                    Text("\(details.capabilities)")
+                        .modifier(
+                            BuildHighlight(canBuild: buildable(details.abbrev))
+                        )
+                }
             }
             .tableStyle(.bordered)
             .border(.blue)
             if let selectedShip {
                 Spacer()
-                ShipTypeView(shipType: shipTypes[selectedShip]!)
+                ShipTypeView(shipType: shipTypes[selectedShip]!, buildable: buildable(shipTypes[selectedShip]!.abbrev))
                     .border(.blue)
             }
             HStack {
@@ -39,6 +75,7 @@ struct ShipTypeReport: View {
 
     struct ShipTypeView: View {
         let shipType: ShipType
+        let buildable: Bool
 
         var body: some View {
             HStack(alignment: .top) {
@@ -66,7 +103,7 @@ struct ShipTypeReport: View {
                     }
                     GridRow {
                         Text("Tech")
-                        Text("\(shipType.tech)")
+                        Text("\(shipType.tech)").foregroundStyle(buildable ? .primary : Color.red)
                     }
                 }
                 Grid(alignment: .leading) {
@@ -131,15 +168,21 @@ struct ShipTypeReport: View {
     }
 }
 
+func preview_buildable(_: String) -> Bool {
+    return true
+}
+
 #Preview {
+
     let ships = [
         "ss": ShipType(
             abbrev: "ss",
             name: "Some Ship",
+            tech: 10,
             speed: 10,
             range: 10,
             capabilities: "300c 10m 900f 15u fish canal"
         )
     ]
-    ShipTypeReport(shipTypes: ships)
+    ShipTypeReport(shipTypes: ships, buildable: preview_buildable)
 }
