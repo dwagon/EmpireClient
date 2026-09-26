dc76d8c5ee56fbb7548b6dd6c404a2f34656858a
Dougal Scott <dougal.scott@gmail.com>
2026-09-26 18:05:01 +1000

    Make map take up half the screen

--- EmpireClient/Views/ContentView.swift
+++ EmpireClient/Views/ContentView.swift
@@ -25,73 +25,80 @@ struct ContentView: View {
     var profile = loadSettings()
 
     var body: some View {
-        NavigationSplitView(columnVisibility: .constant(.doubleColumn)) {
-            Group {
-                if !isLoggedIn {
-                    loginButton
-                } else {
-                    displayMapView
-                        .simultaneousGesture(
-                            TapGesture()
-                                .onEnded {
-                                    tabSelection = .sector
-                                }
-                        )
+        VStack {
+            GeometryReader { geom in
+                HStack {
+                    Group {
+                        if !isLoggedIn {
+                            loginButton
+                        } else {
+                            displayMapView
+                                .simultaneousGesture(
+                                    TapGesture()
+                                        .onEnded {
+                                            tabSelection = .sector
+                                        }
+                                )
+                        }
+                    }
+                    .frame(width: geom.size.width / 2)
+                    tabbedDetailView
+                        .frame(width: geom.size.width / 2)
                 }
-            }.navigationSplitViewColumnWidth(800)
-                .toolbar(removing: .sidebarToggle)
-        } detail: {
-            Spacer()
-            TabView(selection: $tabSelection) {
-                Tab("Sector Details", systemImage: "info", value: .sector) {
-                    SectorDetailView(game: game, centerCoord: centerCoord)
-                }
-                Tab("Ships", systemImage: "sailboat", value: .ship) {
-                    ShipDetailView(game: game, centerCoord: $centerCoord)
-                }.disabled(game.ships.isEmpty)
-                Tab(
-                    "Land Units",
-                    systemImage: "car.rear.road.lane.distance.5",
-                    value: .land
-                ) {
-                    LandDetailView(game: game, centerCoord: $centerCoord)
-                }.disabled(game.landUnits.isEmpty)
-                Tab("Planes", systemImage: "airplane.up.right", value: .plane) {
-                    PlaneDetailView(game: game, centerCoord: $centerCoord)
-                }.disabled(game.planes.isEmpty)
-                Tab("Nukes", systemImage: "sun.max.trianglebadge.exclamationmark", value: .nuke) {
-                    NukeDetailView(game: game, centerCoord: $centerCoord)
-                }.disabled(game.nukes.isEmpty)
             }
-            .onChange(of: tabSelection) { _, newTab in
-                switch newTab {
-                case .land:
-                    Task {
-                        await game.cmd_ldump()
-                    }
-                case .ship:
-                    Task {
-                        await game.cmd_sdump()
-                    }
-                case .plane:
-                    Task {
-                        await game.cmd_pdump()
-                    }
 
-                case .nuke:
-                    Task {
-                        await game.cmd_ndump()
-                    }
-                case .sector:
-                    break
-                }
+            HStack {
+                RawCmdView(game: game).frame(maxWidth: 600)
+                Spacer()
+                LogView(logs: game.logs).scaledToFill()
             }
         }
+    }
 
-        HStack {
-            RawCmdView(game: game).frame(maxWidth: 600)
-            Spacer()
-            LogView(logs: game.logs).scaledToFill()
+    var tabbedDetailView: some View {
+        TabView(selection: $tabSelection) {
+            Tab("Sector Details", systemImage: "info", value: .sector) {
+                SectorDetailView(game: game, centerCoord: centerCoord)
+            }
+            Tab("Ships", systemImage: "sailboat", value: .ship) {
+                ShipDetailView(game: game, centerCoord: $centerCoord)
+            }.disabled(game.ships.isEmpty)
+            Tab(
+                "Land Units",
+                systemImage: "car.rear.road.lane.distance.5",
+                value: .land
+            ) {
+                LandDetailView(game: game, centerCoord: $centerCoord)
+            }.disabled(game.landUnits.isEmpty)
+            Tab("Planes", systemImage: "airplane.up.right", value: .plane) {
+                PlaneDetailView(game: game, centerCoord: $centerCoord)
+            }.disabled(game.planes.isEmpty)
+            Tab("Nukes", systemImage: "sun.max.trianglebadge.exclamationmark", value: .nuke) {
+                NukeDetailView(game: game, centerCoord: $centerCoord)
+            }.disabled(game.nukes.isEmpty)
+        }
+        .onChange(of: tabSelection) { _, newTab in
+            switch newTab {
+            case .land:
+                Task {
+                    await game.cmd_ldump()
+                }
+            case .ship:
+                Task {
+                    await game.cmd_sdump()
+                }
+            case .plane:
+                Task {
+                    await game.cmd_pdump()
+                }
+
+            case .nuke:
+                Task {
+                    await game.cmd_ndump()
+                }
+            case .sector:
+                break
+            }
         }
     }
 
